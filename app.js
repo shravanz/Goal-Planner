85a922a6b4ac335dbe40b8d64d7ec7515c8dfb7d
shravan <shravan.olgikar13@gmail.com>
2017-11-28 00:00:43 +0530

    formatted the code

--- app.js
+++ app.js
@@ -4,28 +4,28 @@ const app = express();
 const exphbs = require('express-handlebars');
 
 //Setting up the Handlebars Middleware
-app.engine('handlebars',exphbs({defaultLayout:'main'}));
-app.set('view engine','handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 
 //Setting up the Index Route
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     const setTitle = 'Welcome to Goal-Planner';
-res.render('index',
-{
-    title:setTitle
-}
-);
+    res.render('index',
+        {
+            title: setTitle
+        }
+    );
 });
 
 //Setting up the About Route
-app.get('/about',(req,res)=>{
+app.get('/about', (req, res) => {
     res.render('about');
 });
 
 //setting up the port to 5000
 const port = 5000;
 //listening to the port on the server start
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
